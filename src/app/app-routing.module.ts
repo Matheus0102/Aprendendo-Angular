cd2cd3675adbace51ce5b9268a576846869f4056
Matheus <matheus.150miranda@gmail.com>
2022-08-17 20:13:58 -0300

    Arquivos Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './Telas/contato/contato.component';
+import { HomeComponent } from './Telas/home/home.component';
+import { LoginComponent } from './Telas/login/login.component';
+import { ProdutosComponent } from './Telas/produtos/produtos.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'Home', component: HomeComponent 
+  },
+  {
+    path:'index', component: HomeComponent
+  },
+  {
+    path:'produtos', component: ProdutosComponent
+  },
+  {
+    path:'Contato', component: ContatoComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './Telas/home/home.component';
+import { LoginComponent } from './Telas/login/login.component';
+import { ProdutosComponent } from './Telas/produtos/produtos.component';
+import { ContatoComponent } from './Telas/contato/contato.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProdutosComponent,
+    ContatoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
