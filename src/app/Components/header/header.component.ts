fff0999b5b9fb95326f5fa031326feef411402d5
Matheus Augusto <matheus.150miranda@gmail.com>
2022-08-29 16:17:19 -0300

    codigo

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  font_size = 16;
+
+  setFontSize(idd: string){
+    if(idd === 'a+'){
+      this.font_size += 1 ;
+    } else if (idd === 'a-') {
+      this.font_size -= 1;
+    } else {
+      this.font_size = 16;
+    }
+
+    let htmlRoot:HTMLElement = <HTMLElement> document.getElementsByTagName("html")[0];
+    if(htmlRoot != null) {
+      htmlRoot.style.fontSize = `${this.font_size}px`;
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
